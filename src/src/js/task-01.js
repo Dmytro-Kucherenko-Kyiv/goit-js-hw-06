aa4df55be2c84fdd63c8e46b6d4138be376c7833
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-02-07 23:12:11 +0200

    step1

---
+++ src/src/js/task-01.js
@@ -0,0 +1,4 @@
+/* 1/ Порахує і виведе в консоль кількість категорій в ul#categories, 
+тобто елементів li.item. */
+
+const list = document.getElementById('categories');
--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+/* const list = document.getElementById('ingredients');
+const liEl = document.createElement('li');
+
+ingredients.forEach(ingredientsElement) 
+{const liEl = (document.createElement('li').innerHTML = 
+ingredientsElement);
+
+  liEl.append(liEl);
+} */
+/* liEl.innerHTML = ingredientsElements; */
+/* list.append(liEl); */
