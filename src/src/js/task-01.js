2db84452a683a2386add67f7a5cf85fd2321857e
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-02-11 20:09:28 +0200

    Update task-01.js

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -1,4 +1,12 @@
 /* 1/ Порахує і виведе в консоль кількість категорій в ul#categories, 
 тобто елементів li.item. */
 
-const list = document.getElementById('categories');
+/* const lists = document.getElementById('categories'); */
+
+/* console.log(list) */
+/* Number of categories =  */
+
+const categories = document.getElementById('categories') 
+  for (let i = 0; i <= categories.lenght-1; i += 1) {
+      console.log(i);
+  }
