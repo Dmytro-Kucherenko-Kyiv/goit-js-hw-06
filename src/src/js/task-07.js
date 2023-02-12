76c5f1d0bf2b8d03321c44cbec2e06f7ac5c967d
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-02-12 23:08:58 +0200

    task_8

---
+++ src/src/js/task-07.js
@@ -0,0 +1,13 @@
+const inputFsText = document.querySelector("#font-size-control");
+const inputText = document.querySelector("#text");
+
+/* inputText.addEventListener('input', (event) => {
+
+    const min = parseInt(inputText.getAttribute("min"));
+    const max = parseInt(inputText.getAttribute("max"));
+
+    for (let fontSize = min; fontSize < max; fontSize += 1) {
+
+    inputText.textContent = event.currentTarget.inputText;
+}
+}) */
