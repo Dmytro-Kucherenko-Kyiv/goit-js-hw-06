25088b9ded3b8e4b791cf5124396e4fa9d60a997
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-02-13 22:20:05 +0200

    finish

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector("#boxes");
+const controls = document.querySelector("#controls").firstElementChild;
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+
+  for (let i = 0; i < controls.value; i += 1) {
+  boxes.innerHTML += `<div style="width:${30 + i * 10}px; height:${30 + i * 10}px; margin-bottom:10px; background-color:${getRandomHexColor()};"></div>`
+  }
+
+} function destroyBoxes() {
+  boxes.innerHTML = ""
+}
